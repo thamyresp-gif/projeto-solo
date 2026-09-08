320d27ea11f2dd9d6fb91475587fa88b403622fb
thamyresp-gif <thamyres_p@hotmail.com>
2026-09-07 23:25:07 -0300

    adiciona tradutor de manês

--- tradutor.py
+++ tradutor.py
@@ -2,3 +2,10 @@
 #Desafio: Tradutor de "Manês"
 #Escreva uma função que recebe uma frase e troca todas as vogais por "aê" (tipo um sotaque de brincadeira). Exemplo:
 
+frase = input("Digite uma frase: ")
+
+for letra in frase:
+    if letra.lower() in "aeiou":
+        print("aê", end="")
+    else:
+        print(letra, end="")
